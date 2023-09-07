3d8420d96a26b7565d895a2a9ccb2baaa16387a9
vokske <kevinmuigai2@gmail.com>
2023-09-07 12:47:09 -0700

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ **mani - Entry point
+ *Return: Alway 0 (If successful)
+ */
+int main (void)
+{
+puts("Programming is like building a multilingual puzzle");
+return 0;
+}	
