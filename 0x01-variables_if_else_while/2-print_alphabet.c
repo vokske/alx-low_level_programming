22de33c60890b67a4201c4542dae2b1dae0401ca
vokske <kevinmuigai2@gmail.com>
2023-09-08 16:52:42 -0700

    Print lowercase letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z'){
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+
+	return (0);
+}
