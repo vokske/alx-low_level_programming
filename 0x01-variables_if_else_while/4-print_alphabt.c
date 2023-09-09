09d76e83e4e4e40dd7c804b6186d8a24dd100afd
vokske <kevinmuigai2@gmail.com>
2023-09-08 18:01:06 -0700

    Omit letters of alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter_lower = 'a';
+
+	while (letter_lower <= 'z')
+	{
+		if (letter_lower != 'e' && letter_lower != 'q')
+		{
+		putchar(letter_lower);
+		}
+		letter_lower++;
+	}
+	putchar('\n');	
+	return (0);
+}
