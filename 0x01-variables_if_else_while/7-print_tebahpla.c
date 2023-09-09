92bab411bffd694f6042b100a0ce256056a5da8b
vokske <kevinmuigai2@gmail.com>
2023-09-08 19:36:59 -0700

    Print lowercase letters in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		--letter;
+	}
+	putchar('\n');
+	return (0);
+}
