b905543d87c827661381bf883cdad2967fba05f7
vokske <kevinmuigai2@gmail.com>
2023-09-11 14:11:29 -0700

    Different combination of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,20 @@
  */
 int main(void)
 {
-	int i;
+	int i, j;
 
-	for(i = 0; i < 100; i++)
+	for (i = 0; i < 10; i++)
 	{
-	putchar(i + '0');
-	if (i != 99)
-	{
-	putchar(',');
-	putchar(' ');
-	}
+		for (j = i + 1; j < 10; j++)
+		{
+		putchar(i + '0');
+		putchar(j + '0');
+		if (i != 8 || j != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		}
 	}
 	putchar('\n');
 	return (0);
