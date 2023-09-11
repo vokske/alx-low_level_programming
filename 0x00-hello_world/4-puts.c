bcad9e4bde1a9afa577d29168f1a69ecf70d3120
vokske <kevinmuigai2@gmail.com>
2023-09-11 15:52:43 -0700

    First C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- **mani - Entry point
- *Return: Alway 0 (If successful)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
