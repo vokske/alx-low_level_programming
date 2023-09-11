62c6f7554e77a08a42da2ffaaf76469dea2f15e7
vokske <kevinmuigai2@gmail.com>
2023-09-11 16:29:10 -0700

    Putchar to print a string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,11 +8,12 @@
  */
 int main(void)
 {
-	const char* str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	const char *x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	int i;
-	for (i = 0; str[i] != '\0'; i++)
+
+	for (i = 0; x[i] != '\0'; i++)
 	{
-	putchar(str[i]);
+	putchar(x[i]);
 	}
 	putchar('\n');
 	return (1);
