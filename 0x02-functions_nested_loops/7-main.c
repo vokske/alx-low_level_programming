9625c86531dd625884c5bea03b809c5bef23ca5d
vokske <kevinmuigai2@gmail.com>
2023-09-13 11:10:51 -0700

    Print last digit

---
+++ 0x02-functions_nested_loops/7-main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int print_last_digit(int n, int m);
+int main()
+{
+print_last_digit('n', 'm');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int print_last_digit(int n, int m)
+{
+m = n % 10;
+_putchar('m');
+return (m);
+}
