5000d81e33e9504edbd06f35e9f9bfd307439dc0
vokske <kevinmuigai2@gmail.com>
2023-09-13 14:31:44 -0700

    Print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,12 +8,12 @@ void jack_bauer(void)
 int h;
 int m;
 
-for ( h = 0; h <= 23; h++)
+for (h = 0; h < 24; h++)
 {
 for (m = 0; m < 60; m++)
 {
 _putchar('0' + (h / 10));
-_putchar('0' + (h & 10));
+_putchar('0' + (h % 10));
 _putchar(':');
 _putchar('0' + (m / 10));
 _putchar('0' + (m % 10));
