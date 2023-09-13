9e7219fb7ab0f83a6a0e085a3e7290d6fef77ab5
vokske <kevinmuigai2@gmail.com>
2023-09-13 10:49:12 -0700

    Compute absoulte value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int_abs - Computes the absolute value of an integer
+ * @n: The integer to be checked
+ * 
+ * Return: The absolute value of integer
+ */
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int _abs(int n);
+int main()
+{
+_abs('n');
+return (0);
+}
