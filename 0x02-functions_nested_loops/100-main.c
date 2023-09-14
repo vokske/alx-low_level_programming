7f40a2b1004a28212de2f4079b1cea8ff22dd533
vokske <kevinmuigai2@gmail.com>
2023-09-14 08:02:05 -0700

    Print the n times table

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_times_table(int n);
+int main()
+{
+print_times_table(5);
+return (0);
+}
---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - Prints the n times table
+ * @n: Integer to be passed
+ */
+void print_times_table(int n)
+{
+if (n >= 0 && n <= 15)
+{
+int i;
+int j;
+
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int product = i * j;
+
+if (j > 0)
+{
+printf(", ");
+}
+if (product < 10)
+{
+printf(" ");
+}
+printf("%d", product);
+}
+printf("\n");
+}
+}
+}
