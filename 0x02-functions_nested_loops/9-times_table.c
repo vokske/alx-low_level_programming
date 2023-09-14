4cbb612c7460066038b37c6e27e15deac294f3d4
vokske <kevinmuigai2@gmail.com>
2023-09-14 03:49:55 -0700

    Print 9s table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,22 +4,24 @@ void times_table(void)
 {
 int i;
 int j;
+int n;
 
 for (i = 0; i <=9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
-_putchar((i + '0'));
+n = i * j;
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+if (j < 9)
+{
+_putchar(',');
 _putchar(' ');
-_putchar('x');
-_putchar(' ');
-_putchar((j + '0'));
-_putchar(' ');
-_putchar('=');
-_putchar(' ');
-_putchar(((i * j) / 10) + '0');
-_putchar(((i * j) % 10) + '0');
 }
+else
+{
 _putchar('\n');
+}
+}
 }
 }
