35bf99fe0253c70685aef4a0ea1793903011a233
vokske <kevinmuigai2@gmail.com>
2023-09-14 05:08:33 -0700

    Print 9s table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
+/**
+ * times_table - Prints the 9s table
+ */
+
 void times_table(void)
 {
 int i;
 int j;
 int n;
 
-for (i = 0; i <=9; i++)
+for (i = 0; i <= 9; i++)
 {
 for (j = 0; j <= 9; j++)
 {
