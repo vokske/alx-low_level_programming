297a9f6a7f36a9541cbe11ea5674ebc2fa67b482
vokske <kevinmuigai2@gmail.com>
2023-09-14 19:26:59 -0700

    Draw a straight line

---
+++ 0x04-more_functions_nested_loops/6-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+print_line(6);
+return (0);
+}
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Prints a straight line
+ * @n: Parameter to be passed
+ *
+ */
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i <= n; i++)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+_putchar('_');
+}
+_putchar('\n');
+}
