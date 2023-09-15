107783d81a2e35a171f4777ce8037ed493c81748
vokske <kevinmuigai2@gmail.com>
2023-09-14 19:06:17 -0700

    Print 0-14 ten times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,11 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j <= 14; j++)
 {
-if (j <= 9)
-{
-_putchar(j + '0');
-}
-else
+if (j > 9)
 {
 _putchar((j / 10) + '0');
-_putchar((j % 10) + '0');
 }
+_putchar((j % 10) + '0');
 }
 _putchar('\n');
 }
