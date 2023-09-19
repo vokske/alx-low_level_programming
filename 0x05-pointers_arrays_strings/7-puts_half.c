f17a52d3c09af5f1e2454a1cd1266dac32817572
vokske <kevinmuigai2@gmail.com>
2023-09-19 07:48:11 -0700

    Print the second half of string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,15 +8,8 @@ while (str[len] != '\0')
 {
 len++;
 }
-if (len % 2 == 0)
-{
-mid = len / 2;
-}
-else
-{
-mid = ((len - 1) / 2);
-}
-for (i = mid; i <= len; i++)
+mid = len % 2 == 0 ? len / 2 : (len + 1) / 2;
+for (i = mid; i < len; i++)
 {
 _putchar(str[i]);
 }
