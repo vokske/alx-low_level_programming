5f8524dad31ef72742a4a48be94ae95ba7ae13e1
vokske <kevinmuigai2@gmail.com>
2023-09-19 09:44:14 -0700

    Copy string using pointers

---
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+char scr[] = "Be, and let be";
+char dest[50];
+_strcpy(dest, scr);
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**_strcpy - Copies a string to memory
+ * @dest: Memory address
+ * @scr: Another memory location
+ *
+ * Return: Value of the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+char *orig_dest = dest;
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+*dest = '\0';
+return (orig_dest);
+}
