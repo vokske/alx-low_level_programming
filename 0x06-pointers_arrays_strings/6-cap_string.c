e82aee6a551d2af79f4c68fac68d1646ccf4386a
vokske <kevinmuigai2@gmail.com>
2023-09-21 14:24:40 -0700

    COncatenate strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdbool.h>
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @s: Random string
+ *
+ * Return: Capitalized string
+ */
+bool is_separator(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' || c == ',' || c == ';' || c == '.' || c == '!' || c == '?' || c == '"' || c == '(' || c == ')' || c == '{' || c == '}');
+}
+char *cap_string(char *s)
+{
+
+
+}
+
+
