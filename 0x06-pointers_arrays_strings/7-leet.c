ae683f51d42776ccd7b2216b7d0c19338a06d425
vokske <kevinmuigai2@gmail.com>
2023-09-22 11:43:52 -0700

    Encode a string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * leet - Encodes a string into 1337
+ *
+ * Return: Encoded string
+ */
+char *leet(char *s)
+{
+	char *orig = s;
+	
+	while (*s)
+	{
+	if (*s == 'a' || *s == 'A')
+	{
+	*s = '4';
+	}
+	else if (*s == 'e' || *s == 'E')
+	{
+	*s = '3';
+	}
+	else if (*s == 'o' || *s == 'O')
+	{
+	*s = '0';
+	}
+	else if (*s == 't' || *s == 'T')
+	{
+	*s = '7';
+	}
+	else if (*s == 'l' || *s == 'L')
+	{
+	*s = 1;
+	}
+	s++;
+	}
+	return (orig);
+}
---
+++ 0x06-pointers_arrays_strings/7-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	char s[] = "Lotter";
+	leet(s);
+	return (0);
+}
