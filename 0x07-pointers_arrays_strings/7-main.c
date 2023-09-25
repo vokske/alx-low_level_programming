26f60efea54d6b5daa7ffa4cbd4b8f72cdacba7a
vokske <kevinmuigai2@gmail.com>
2023-09-25 15:18:04 -0700

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	char chessboard[8][8] = {
+	{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+	{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+	{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+	{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'}
+	};
+	print_chessboard(chessboard);
+	return (0);
+}
