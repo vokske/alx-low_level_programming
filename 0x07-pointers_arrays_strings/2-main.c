ba919286d2e9d2169cf455a6e9295fbb9827b73e
vokske <kevinmuigai2@gmail.com>
2023-09-25 10:57:26 -0700

    Locate a character in string

---
+++ 0x07-pointers_arrays_strings/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	char str[10] = "cris-cross";
+	_strchr(str, 'c');
+	return (0);
+}
---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: Random string
+ * @c: Character in the string
+ *
+ * Return: Pointer to the first occurrence of desired character
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return NULL;
+}
