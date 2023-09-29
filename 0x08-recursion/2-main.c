cddd81928ca52f78eb0da8bf7b2414a5104aea60
vokske <kevinmuigai2@gmail.com>
2023-09-29 14:06:49 -0700

    Find length of string recursively

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	char s[] = "Caroline";
+	_strlen_recursion(s);
+	_putchar('\n');
+	return (0);
+}
