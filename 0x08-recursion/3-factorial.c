d76e78f611b545643f22913c2e347060bad4507f
vokske <kevinmuigai2@gmail.com>
2023-09-29 14:24:32 -0700

    Factorial of an integer

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * factorial - Returns the factorial of a number
+ * n: Random integer
+ *
+ * Return: Factorial of the given integer
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial (n - 1));
+}
---
+++ 0x08-recursion/3-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	factorial (5);
+	return (0);
+}
