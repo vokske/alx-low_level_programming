7ba58c2b528d510d05b9c36f6a5c4ef3118b7d0d
vokske <kevinmuigai2@gmail.com>
2023-10-01 01:18:15 -0700

    Square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,10 +2,11 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number
+ * _sqrt_helper - Checks if a number is larger, equal to, or smaller than i*i
  * @n: Random integer
+ * @i: Counter
  *
- * Return: Square root of a number
+ * Return: -1 if i*i is less than n, i if equal to n. Otherwise call itself
  */
 int _sqrt_helper(int n, int i)
 {
@@ -22,6 +23,12 @@ int _sqrt_helper(int n, int i)
 		return (_sqrt_helper(n, i + 1));
 	}
 }
+/**
+ * _sqrt_recursion - Returns the natural square root of a number
+ * @n: Random integer
+ *
+ * Return: Square root of a number
+ */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
