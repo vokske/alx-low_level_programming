ecd49181183cded7f6b9a5aa5efdb854fa4a3542
vokske <kevinmuigai2@gmail.com>
2023-10-02 06:10:03 -0700

    Check for identical strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -38,7 +38,7 @@ int wildcmp(char *s1, char *s2)
 		{
 			return (0);
 		}
-		return (wildcmp(s1 + 1, s2) || wildcmp(s2, s1 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	return (0);
 }
