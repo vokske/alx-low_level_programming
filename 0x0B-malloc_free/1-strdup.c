e88a044dfd56e2181237d1e2d5d3233e93fd8ee7
vokske <kevinmuigai2@gmail.com>
2023-10-07 03:04:34 -0700

    Duplicate string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,7 +11,7 @@ char *_strdup(char *str)
 {
 	int len = 0;
 	int i;
-	char *dup_str = malloc ((len + 1) * sizeof(char));
+	char *dup_str = malloc((len + 1) * sizeof(char));
 
 	if (str == NULL)
 	{
@@ -28,6 +28,7 @@ char *_strdup(char *str)
 	for (i = 0; i <= len; i++)
 	{
 		dup_str[i] = str[i];
+		dup_str[len] = '\0';
 	}
 	return (dup_str);
 }
