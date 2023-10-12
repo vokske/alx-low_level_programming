d75647340a68cfde4f9575588f97371d6d44eaf3
vokske <kevinmuigai2@gmail.com>
2023-10-12 06:17:57 -0700

    Concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,16 +14,19 @@ char *str_concat(char *s1, char *s2)
 	int len1, len2;
 	char *new_str;
 
-	for (len1 = 0; s1 && s1[len1]; len1++);
-	for (len2 = 0; s2 && s2[len2]; len2++);
-	new_str = malloc(sizeof(char) * (len1 + len2 + 1));
+	for (len1 = 0; s1 && s1[len1]; len1++)
+	{
+		for (len2 = 0; s2 && s2[len2]; len2++)
+		{
+			new_str = malloc(sizeof(char) * (len1 + len2 + 1));
+		}
+	}
 	if (!new_str)
 	{
 		return (NULL);
 	}
 	for (len1 = 0; s1 && s1[len1]; len1++)
 	{
-
 		new_str[len1] = s1[len1];
 	}
 	for (len2 = 0; s2 && s2[len2]; len2++)
