428bd1edcf619971bf9ca601581d5daa32fb88b2
vokske <kevinmuigai2@gmail.com>
2023-10-13 03:46:25 -0700

    Free grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * free-grid - Frees a 2-D grid previously created
+ * @grid: 2-D array
+ * @height: Array columns
+ *
+ * Return: Nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height <= 0)
+	{
+		return;
+	}
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
---
+++ 0x0B-malloc_free/4-main.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int height = 5;
+	int width = 5;
+
+	int grid = (int **)alloc_grid(width, height);
+	if (grid == NULL)
+	{
+		printf("Failed to allocate grid.\n");
+		return (1);
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			printf("%d", grid[i][j]);
+		}
+		printf("\n");
+	}
+	free_grid(grid, height);
+	return (0);
+}
