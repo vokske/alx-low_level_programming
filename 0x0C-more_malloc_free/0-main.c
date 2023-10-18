413170ba621ff54b5852943fe003e1ee6a456f98
vokske <kevinmuigai2@gmail.com>
2023-10-18 05:44:59 -0700

    Allocate memory

---
+++ 0x0C-more_malloc_free/0-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned int num = 10;
+
+	void *ptr = malloc_checked(num * sizeof(unsigned int));
+	free(ptr);
+	return (0);
+}
---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * malloc_checked - Allocates memory
+ * @b: Random integer
+ *
+ * Return - Nothing
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b * sizeof(unsigned int));
+
+	if (ptr == NULL)
+	{
+		printf("Memory allocation failed.\n");
+		exit (98);
+	}
+	return (ptr);
+}
