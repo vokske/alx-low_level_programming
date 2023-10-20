9c3f88913ca2882ee2970607214369d26ac79d62
vokske <kevinmuigai2@gmail.com>
2023-10-20 06:43:33 -0700

    Struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dog - New type named dog
+ * @name: First member
+ * @owner: Second member
+ * @age: Third member
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
