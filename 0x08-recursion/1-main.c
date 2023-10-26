f8aea272e8c8db745984e958bbe8e85385e75c63
vokske <kevinmuigai2@gmail.com>
2023-10-26 13:40:09 -0700

    Print string in reverse

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char s[] = "Hey there!";
+	
+	_print_rev_recursion(s);
+	return (0);
+}
