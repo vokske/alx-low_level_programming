61f0b25550f842fe99830c9ed9e4356fbba5f781
vokske <kevinmuigai2@gmail.com>
2023-10-27 01:33:33 +0300

    Power of a number

---
+++ 0x08-recursion/4-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	int x = 4;
+	int y = 3;
+
+	_pow_recursion(x, y);
+	return (0);
+}
---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _pow_recursion - Evaluates x raised to the power of y
+ * @x:Base number
+ * @y: Power
+ *
+ * Return: Value of x raised to power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * (_pow_recursion(x, y - 1)));
+	}
+}
