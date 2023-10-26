94fa01a79ef2f556e3e3182ba3a0dd002230ed4b
root <root@Voks>
2023-10-27 00:52:36 +0300

    Find string length

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,15 +10,12 @@
  */
 int _strlen_recursion(char *s)
 {
-	int len;
-
 	if (*s == '\0')
 	{
 		return (0);
 	}
 	else
 	{
-		len = _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
-	return (len);
 }
