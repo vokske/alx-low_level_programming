ee90e39de19222ac51205319819ccfae866cd0ce
vokske <kevinmuigai2@gmail.com>
2023-10-27 17:12:49 +0300

    Find prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * divisibility - Checks for the number of divisors
+ * @n: Random number
+ * @divisor: Divisors of n
+ *
+ * Return: Number of divisors
+ */
+int divisibility(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (divisibility(n, i - 1));
+	}
+}
+/**
+ * is_prime_number - Checks for prime number
+ * @n: Random number
+ *
+ * Return: 1 if n is prime number. 0 otherwise
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	return (divisibility(n, n / 2));
+}
---
+++ 0x08-recursion/6-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	int n = 7;
+
+	is_prime_number(n);
+	return (0);
+}
