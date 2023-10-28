0d35ccbc3670071e98eed23ea3dd99f6ad19dd36
vokske <kevinmuigai2@gmail.com>
2023-10-28 15:58:32 +0300

    First commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - Entry point
+ * @argc: Argument counter
+ * @argv: Arguments to the program
+ *
+ * Return: Always 0
+ */
 int main(int argc, char *argv[])
 {
 	int i;
-	
-	for(i = 0; i < argc; i++)
-	{	
+
+	for (i = 0; i < argc; i++)
+	{
 	printf("%s\n", argv[0]);
 	}
 	return (0);
