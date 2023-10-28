f7653e7f4885361936d30abe8d8bc69b559a4456
vokske <kevinmuigai2@gmail.com>
2023-10-28 16:44:14 +0300

    Print arguments to a program

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument counter
+ * @argv: Arguments to program
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
