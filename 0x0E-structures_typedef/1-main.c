292290e4ed7098b4857c219cc06e83822862d98d
vokske <kevinmuigai2@gmail.com>
2023-11-08 17:25:01 +0300

    Function to initialize a struct

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -9,11 +9,11 @@
 void init_dog(struct dog *d, char *name, float age, char *owner);
 int main()
 {
-	struct dog my_dog;
-	init_dog(&my_dog, "Rex", 4.5, "Voks");
+	struct dog dog;
+	init_dog(&dog, "Rex", 4.5, "Voks");
 
-	printf("Dog's name:%s\n", my_dog.name);
-	printf("Dog's age: %.1f\n", my_dog.age);
-	printf("Dog's owner: %s\n", my_dog.owner);
+	printf("Dog's name:%s\n", dog.name);
+	printf("Dog's age: %.1f\n", dog.age);
+	printf("Dog's owner: %s\n", dog.owner);
 	return (0);
 }
