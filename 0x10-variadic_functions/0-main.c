3d2dcf735c3ec969b2977cec10d1b557ca06c625
vokske <kevinmuigai2@gmail.com>
2023-11-09 15:26:23 +0300

    Function to print a name

--- 0x10-variadic_functions/0-main.c
+++ 0x10-variadic_functions/0-main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main()
 {
-	sum_them_all (3, 2, 5, 8);
+	sum_them_all(2, 3, 45);
 	return (0);
 }
