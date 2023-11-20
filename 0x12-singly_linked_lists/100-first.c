ae016ab23ecb4bfc80416b6d162af1f59699d1cf
vokske <kevinmuigai2@gmail.com>
2023-11-20 14:56:25 +0300

    Printing before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * before_main - Prints before the main function
+ *
+ * Return: Nothing
+ */
 void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
