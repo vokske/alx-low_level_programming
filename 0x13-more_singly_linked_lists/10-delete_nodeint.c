8b0ec885ebc4cfdc71604e612dea24932b75c182
vokske <kevinmuigai2@gmail.com>
2023-11-21 18:26:43 +0300

    Delete node at a certain index

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - Deletes node at certain index in a list
+ * @head: Pointer to a pointer of the first node of list
+ * @index: Location of the node to be deleted
+ *
+ * Return: 1 if successful. -1 otherwise.
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *temp, *node_del;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+
+	temp = *head;
+
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
+
+	while (i < index - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	node_del = temp->next;
+	temp->next = node_del->next;
+	node_del->next = NULL;
+	free(node_del);
+
+	return (1);
+}
