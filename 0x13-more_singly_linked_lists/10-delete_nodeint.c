6a23c9e855b85663b3daa16661a310f6e7c752d4
vokske <kevinmuigai2@gmail.com>
2023-11-23 03:12:41 +0300

    Delete node at a certain index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -31,11 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	while (i < index - 1)
 	{
 		temp = temp->next;
-		i++;
-	}
-	if (index > i)
-	{
-		return (-1);
+		i++;;
 	}
 
 	if (temp == NULL || temp->next == NULL)
