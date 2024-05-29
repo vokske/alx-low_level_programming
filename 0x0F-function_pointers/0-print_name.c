f5eeaa29ee7b40dce7cfdc3a3002307d237e879d
vokske <kevinmuigai2@gmail.com>
2024-05-29 19:39:35 +0300

    Print a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,10 @@
 #include "function_pointers.h"
 #include <stdio.h>
 
-void f(char *s)
-{
-	int i = 0;
-	while(s[i] != '\0')
-	{
-		_putchar(s[i]);
-		i++;
-	}
-	_putchar('\n');
-}
-
 void print_name(char *name, void (*f)(char *))
 {
-
-	f(name);
+	if(name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
