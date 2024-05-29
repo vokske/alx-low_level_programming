3f16a680f9b81e843406ea35b0e3445d05ea74b7
vokske <kevinmuigai2@gmail.com>
2024-05-29 21:25:51 +0300

    Execute a function given as parameter

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
@@ -5,4 +8,5 @@ void _putchar(char c);
 void f(char *s);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
+void print_element(int element);
 #endif
