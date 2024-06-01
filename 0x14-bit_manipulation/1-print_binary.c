f854e37929037a00e05eb58f7104caec30cc0a9a
vokske <kevinmuigai2@gmail.com>
2024-06-01 05:52:32 +0300

    Decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of an integer
+ * @n: Integer to be converted to binary
+ *
+ * Return: Nothing.
+ */
+void print_binary(unsigned long int n)
+{
+	int current, i;
+
+	if(n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	for (i = sizeof(n) * 8 - 1; i >= 0; i--)
+	{
+
+		if (n & (1UL << i))
+		{
+			current = 1;
+		}
+		if (current)
+		{
+			if (n & (1UL << i))
+			{
+				_putchar('1');
+			}
+			else
+			{
+				_putchar('0');
+			}
+		}
+	}
+	_putchar('\n');
+}
