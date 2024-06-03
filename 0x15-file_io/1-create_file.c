72b2d8bbc01efc71dda4f3e6c7916170c3887cdf
vokske <kevinmuigai2@gmail.com>
2024-06-03 17:20:45 +0300

    Create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		while(text_content[i])
+		while (text_content[i])
 			i++;
 		bytes_wrt = write(fl, text_content, i);
 		if (bytes_wrt == -1)
@@ -29,5 +29,5 @@ int create_file(const char *filename, char *text_content)
 	}
 	close(fl);
 
-	return (1);	
+	return (1);
 }
