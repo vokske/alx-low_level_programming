a4c6ec16c87bb98e3e444bb3ac43a3e319f27cb2
vokske <kevinmuigai2@gmail.com>
2024-06-04 20:51:48 +0300

    Copy content from one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,7 @@
 int main(int argc, char *argv[])
 {
 	int fd_r, fd_w, r, w, cl_r, cl_w;
-	char *buff[1024];
+	char buff[1024];
 
 	if (argc != 3)
 	{
@@ -21,18 +21,31 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (argv[2])
-		fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	r = read(fd_r, buff, 1024);
-	w = write(fd_w, buff, r);
-	while (r)
+	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd_w == -1)
+                {
+                        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		       	close(fd_r);
+			exit(99);
+		}
+
+	while ((r = read(fd_r, buff, 1024)) > 0)
 	{
-		if (fd_w == -1 || w != r)
+		w = write(fd_w, buff, r);
+		if (w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			close(fd_r);
+			close(fd_w);
 			exit(99);
 		}
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		close(fd_r);
+		close(fd_w);
+		exit(98);
 	}
 	cl_r = close(fd_r);
 	cl_w = close(fd_w);
@@ -40,10 +53,15 @@ int main(int argc, char *argv[])
 	if (cl_r == -1 || cl_w == -1)
 	{
 		if (cl_r == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
+			exit(100);
+		}
 		if (cl_w == -1)
+		{
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
-		exit(100);
+			exit(100);
+		}
 	}
 	return (0);
 }
