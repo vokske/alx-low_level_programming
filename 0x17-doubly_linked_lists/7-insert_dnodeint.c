cdd4fd47e8cb4d34af5fccd411fb87d35f0d90a8
vokske <kevinmuigai2@gmail.com>
2024-06-05 07:55:05 +0300

    Insert node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <stddef.h>
 
 /**
  * insert_dnodeint_at_index - inserts a new node at a given position
@@ -20,43 +19,37 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	if (idx != 0)
-	{
-		temp = *h;
-		for (counter = 0; counter < idx - 1 && temp != NULL; counter++)
-		{
-			temp = temp->next;
-		}
-		if (temp == NULL)
-		{
-			return (NULL);
-		}
-	}
 	new_node = malloc(sizeof(dlistint_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	new_node->n = n;
+
 	if (idx == 0)
 	{
-		new_node->next = *h;
 		new_node->prev = NULL;
+		new_node->next = *h;
+
 		if (*h != NULL)
 		{
 			(*h)->prev = new_node;
 		}
 		*h = new_node;
+		return (new_node);
 	}
-	else
-	{
-		new_node->next = temp->next;
-		new_node->prev = temp;
-		if (temp->next != NULL)
-		{
-			temp->next = new_node;
-			new_node->prev = temp;
-		}
-	}
+	temp = *h;
+	for (counter = 0; counter < idx - 1 && temp != NULL; counter++)
+		temp = temp->next;
+	if (temp->next == NULL || (temp->next == NULL && counter < idx - 1))
+		return (NULL);
+
+	new_node->next = temp->next;
+	new_node->prev = temp;
+	if (temp->next != NULL)
+		temp->next->prev = new_node;
+	temp->next = new_node;
+
 	return (new_node);
 }
