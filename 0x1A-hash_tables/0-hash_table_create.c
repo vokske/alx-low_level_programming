dfec236b97c79f2e490c5f70a415e46170a9cee2
vokske <kevinmuigai2@gmail.com>
2024-06-12 19:35:38 +0300

    Create Hash Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 
-	table->array = malloc(sizeof(hash_table_t) * size);
+	table->array = malloc(sizeof(hash_table_t *) * size);
 
 	if (table->array == NULL)
 	{
