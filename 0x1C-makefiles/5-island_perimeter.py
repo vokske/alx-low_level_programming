4861d28da1d8cfb8e2cfef8761c4e3b35685975b
vokske <kevinmuigai2@gmail.com>
2024-09-10 06:32:33 +0300

    Technical quiz

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module represents island_perimeter."""
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid."""
+     x = len(grid)
+     y = len(grid[0])
+     perimeter = 0
+
+    for i in range(x):
+        for j in range(y):
+            if grid[i][j] == 1:
+                perimeter = perimeter + 4
+                if grid[i][j+1] or grid[i+1][j] == 1:
+                    perimeter = perimeter - 2
+                if grid[i][j+1] == 1 and grid[i+1][j] == 1:
+                    perimeter = perimeter -2
+    return perimeter
